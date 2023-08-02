27e4ad12a91d4dc0a5c80b408f2debce4f0f5cd1
mort1k <mort1k@DESKTOP-TSCUD11.localdomain>
2023-08-02 04:08:52 +0200

    Show amount function has become overcomplicated, committing before rebuilding.

--- calculations.js
+++ calculations.js
@@ -4,26 +4,74 @@
 
 const province = {
     name: "Alberta",
-    gst: 2,
-    pst: 3,
-    hst: 15,
+    gst: 11,
+    pst: 13,
+    hst: 9,
 };
+
+
+function calculateAdd(amountObject, tax) {
+    if (amountObject.amount > 0) {
+        return amountObject.amount * (tax/100);
+    } else {
+        return 0;
+    };
+};
+
+
+function calculateDeduct(amountObject, tax) {
+    if (amountObject.amount > 0) {
+        return amountObject.amount / (1+tax/100);
+    } else {
+        return 0;
+    }
+}
+
 
 const amount = {
     amount: 15,
-    gst: 0,
-    pst: 0,
-    hst: 0,
-    total: 0,
     operation: "add",
+    addGst: function(provinceObject) {
+        return calculateAdd(this, provinceObject.gst);
+    },
+    addPst: function(provinceObject) {
+        return calculateAdd(this, provinceObject.pst);
+    },
+    addHst: function(provinceObject) {
+        return calculateAdd(this, provinceObject.hst);
+    },
+    addTotal: function() {
+        if (this.amount > 0) {
+            return this.amount + this.addGst(province) 
+            + this.addPst(province) + this.addHst(province);
+        } else {
+            return 0;
+        };
+    },
+    deductGST: function(provinceObject) {
+        return calculateDeduct(this, provinceObject.gst);
+    },
+    deductPST: function(provinceObject) {
+        return calculateDeduct(this, provinceObject.pst);
+    },
+    deductHST: function(provinceObject) {
+        return calculateDeduct(this, provinceObject.hst);
+    },
+    deductTotal: function() {
+        if (this.amount > 0) {
+            return this.amount - this.gst() - this.pst() - this.hst();
+        } else {
+            return 0;
+        };
+    },
 };
 
-function showProvince() {
+function showProvince(provinceObject) {
     document.getElementById("province").innerHTML = province.name;
     let gst = document.getElementById("gst");
     let pst = document.getElementById("pst");
     let hst = document.getElementById("hst");
-    const rates = [province.gst, province.pst, province.hst];
+    const rates = [provinceObject.gst, provinceObject.pst, provinceObject.hst];
     const displays = [gst, pst, hst];
     const texts = ["% GST", "% PST", "% HST"]
     for (let i = 0; i < rates.length; i++) {
@@ -40,7 +88,7 @@ function showProvince() {
     };
 };
 
-
+/*
 function showAmount() {
     let amountText = document.getElementById("amount-text");
     let amountAmount = document.getElementById("amount-amount");
@@ -97,3 +145,56 @@ function showAmount() {
         };
     };
 };
+*/
+
+// Showing amount based on object input
+
+function showAmount(amountObject, provinceObject) {
+    let amountText = document.getElementById("amount-text");
+    let amountInput = document.getElementById("amount-amount");
+    if (amountObject.amount > 0) {
+        let totalAmount = document.getElementById("total-amount");
+        let operator = "";
+        const taxes = [];
+        const texts = [document.getElementById("gst-text"), 
+            document.getElementById("pst-text"), document.getElementById("hst-text")];
+        const amounts = [document.getElementById("gst-amount"), 
+            document.getElementById("pst-amount"), document.getElementById("hst-amount")];
+        const rates = [provinceObject.gst, provinceObject.pst, provinceObject.hst];
+        const strings = ["% GST:", "% PST:", "% HST:"];
+        amountText.innerText = "Amount:";
+        amountInput.innerText = amountObject.amount + "$";
+        if (amountObject.operation === "add") {
+            operator = "+ ";
+            taxes.push(amountObject.addGst(province), amountObject.addPst(province), amountObject.addHst(province));
+            totalAmount.innerText = amountObject.addTotal() + "$";
+        } else {
+            taxes.push(amountObject.deductGst(province), 
+            amountObject.deductPst(province), amountObject.deductHst(province));
+            if (amountObject.operation === "deduct") {
+                operator = "- ";
+                totalAmount.innerText = amountObject.deductTotal();
+            }   else if (amountObject.operation === "calculate") {
+                operator= "incl. ";
+                totalAmount.innerText = amountObject.amount;
+            }
+        }
+        for (let i = 0; i < taxes.length; i++) {
+            if (taxes[i] > 0) {
+                texts[i].innerText = operator + rates[i] + strings[i];
+                amounts[i].innerText = taxes[i].toFixed(2) + "$";
+            } else {
+                texts[i].innerText = "";
+                amounts[i].innerText = "";
+            };
+        };
+        if (taxes.length > 0) {
+            document.getElementById("total-text").innerText = "Total:";
+        } else {
+            document.getElementById("total-text").innerText == "";
+        };
+    } else {
+        amountText.innerText = "";
+        amountInput.innerText = "";
+    }
+}
--- navigation.js
+++ navigation.js
@@ -2,13 +2,44 @@
 
 // Drop-down menus
 
+document.addEventListener("click", hideMenus);
+
+function switchClasses(classRemove, classAdd, id) {
+    let element = document.getElementById(id);
+    if (element.classList.contains(classRemove)) {
+        element.classList.remove(classRemove);
+        element.classList.add(classAdd);
+    };
+};
+
+
+function showMenu(id) {
+    switchClasses("drop-down-hidden", "drop-down-visible", id);
+}
+
+
+function hideMenu(id) {
+    switchClasses("drop-down-visible", "drop-down-hidden", id)
+}
+
+
 function dropMenu(id) {
     let menu = document.getElementById(id);
     if (menu.classList.contains("drop-down-hidden")) {
-        menu.classList.remove("drop-down-hidden");
-        menu.classList.add("drop-down-visible");
+        showMenu(id);
     } else {
-        menu.classList.remove("drop-down-visible");
-        menu.classList.add("drop-down-hidden");
+        hideMenu(id);
     };
 };
+
+
+function hideMenus(event) {
+    let menuProvinces = document.getElementById("container-provinces");
+    let menuTip = document.getElementById("container-tip");
+    if (!menuProvinces.contains(event.target)) {
+        hideMenu("drop-down-provinces");
+    }
+    if (!menuTip.contains(event.target)) {
+        hideMenu("drop-down-tip");
+    }
+}
